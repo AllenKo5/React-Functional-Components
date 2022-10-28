068141bd5ed9bea0f45a0f2085bdfc4ce911a375
Allen Ko (RIT Student) <ask9458@rit.edu>
2022-10-28 15:42:34 -0400

    set up all examples

---
+++ client/example3.jsx
@@ -0,0 +1,40 @@
+const { useState, useEffect } = React;
+
+const SongContainer = (props) => {
+    const [songs, setSongs] = useState(props.songs);
+
+    useEffect(async () => {
+        const response = await fetch('/getSongs');
+        const songs = await response.json();
+        setSongs(songs);
+    }, []);
+
+    if (songs.length === 0) {
+        return (
+            <div>
+                <h3>No Songs Yet!</h3>
+            </div>
+        );
+    }
+
+    const songList = songs.map(song => {
+        return (
+            <div key={song.title}>
+                <h2>{song.artist} - <i>{song.title}</i></h2>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            <h2>My Favorite Songs!</h2>
+            {songList}
+        </div>
+    );
+};
+
+const init = () => {
+    ReactDOM.render(<SongContainer songs={[]} />, document.getElementById('app'));
+};
+
+window.onload = init;
